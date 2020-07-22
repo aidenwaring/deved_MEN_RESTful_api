bf37f14fa7be4914432c44a9fef6c2b3e0ec12e5
aidenwaring <aiden.waring@icloud.com>
2020-07-22 17:24:19 +1000

    Deconstructed env variables for connection

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
+const app = express()
 
-const bodyParser = require('body-parser')
+const PORT = 3000;
+
+// const bodyParser = require('body-parser')
 require('dotenv/config')
 
 //Ensures that the app uses bodyParser to take JSON data from requests (POSTS)
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 //Middleware
 // app.use('/posts', () => {
@@ -22,10 +26,19 @@ app.use('/', homeRoute)
 ////////////////
 
 //Connect to DB
+// mongoose.connect(
+//     process.env.DB_CONNECTION, {useNewURLParser: true}, () =>{
+//     console.log('Connected to DB')
+// })
+
 mongoose.connect(
-    process.env.DB_CONNECTION, {useNewURLParser: true}, () =>{
-    console.log('Connected to DB')
-})
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u1mch.mongodb.net/<dbname>?retryWrites=true&w=majority`,(err)=>{
+        if(err) throw err
+        console.log("DB Connected Successfully")
+    }
+)
 
 //How we start listening to the server
-app.listen(3000)
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
